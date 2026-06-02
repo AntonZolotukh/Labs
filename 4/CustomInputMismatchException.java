6500b9cc791f0b89cf70e11220adcb925796b716
AntonZolotukh <atom9522@gmail.com>
2026-06-03 01:41:27 +0300

    Лабораторные работы 1-8

---
+++ 4/CustomInputMismatchException.java
@@ -0,0 +1,13 @@
+public class CustomInputMismatchException extends Exception {
+    public CustomInputMismatchException() {
+        super();
+    }
+
+    public CustomInputMismatchException(String message) {
+        super(message);
+    }
+
+    public CustomInputMismatchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
